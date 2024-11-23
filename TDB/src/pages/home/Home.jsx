d5d5288bcc33e47244ae63db15c2d67b09706ccf
dkswnn <onolbaatardulguun5@gmail.com>
2024-11-23 17:28:26 +0800

    Router bolon pagelayout yanzlav

---
+++ TDB/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import PageLayout from "../../layouts/PageLayout";
+
+const Home = () => {
+  return (
+    <PageLayout>
+      <div>this is home</div>
+    </PageLayout>
+  );
+};
+
+export default Home;
